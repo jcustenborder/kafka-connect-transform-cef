b533c9cfbc60aa4587d0181641cdfb0831b3476a
Jeremy Custenborder <jcustenborder@gmail.com>
2017-05-02 10:53:18 -0500

    Changed the parsing logic to match changes from the cef parsing library. Cleanup of formatting.

--- src/main/java/com/github/jcustenborder/kafka/connect/transform/cef/CEFTransformation.java
+++ src/main/java/com/github/jcustenborder/kafka/connect/transform/cef/CEFTransformation.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -60,7 +60,12 @@ public class CEFTransformation<R extends ConnectRecord<R>> implements Transforma
     try {
       log.trace("apply() - parsing '{}'", inputMessage);
       Message message = parser.parse(inputMessage);
-    } catch (IllegalStateException ex) {
+      if (null == message) {
+        log.trace("apply() - Could not parse message. Returning original record. message = '{}'", inputMessage);
+        return record;
+      }
+    } catch (Exception ex) {
+      log.warn("Exception thrown while parsing message.", ex);
       return record;
     }
 
